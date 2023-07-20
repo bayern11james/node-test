ee6f53901d758aa463865c5661de7226af071c01
李天缘 <1094934623@qq.comm>
2023-07-20 17:31:03 +0800

    first commit

---
+++ call_module.js
@@ -0,0 +1,3 @@
+var module = require("./module.js");
+console.log(module.method1())
+console.log(module.method2())
---
+++ module.js
@@ -0,0 +1,7 @@
+exports.method1 = function(){
+  return "hello world"
+}
+
+exports.method2 = function(){
+  return "fuck world"
+}
